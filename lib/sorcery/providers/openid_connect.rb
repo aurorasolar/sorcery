b899bed2b57be0dccfd4f7f52167678864afafbd
Jason Shum <js@shumjason.com>
2015-11-30 18:37:33 -0800

    Add support for state in OpenID connect provider
    Signed-off-by: Jason Shum <js@shumjason.com>

--- lib/sorcery/providers/openid_connect.rb
+++ lib/sorcery/providers/openid_connect.rb
@@ -9,9 +9,9 @@ module Sorcery
     #   ...
     #
     class Openid_connect < Base
-      
 
-      attr_accessor :auth_path, :scope, :token_url, :user_info_path, 
+
+      attr_accessor :auth_path, :scope, :token_url, :user_info_path,
       :discovery_document_url, :use_discovery_document, :jwks_uri, :client_auth_in_body,
       :use_stored_jwk, :stored_jwk
 
@@ -25,16 +25,16 @@ module Sorcery
       end
 
       def get_user_hash(tokens)
-        
+
         access_token = tokens[:access_token]
         id_token = tokens[:id_token]
 
         return_hash = {}.tap do |rh|
-          rh[:access_token] = access_token.access_token          
+          rh[:access_token] = access_token.access_token
           rh[:uid] = id_token.sub
           rh[:user_info] = id_token.raw_attributes.tap do |uh|
             uh[:access_token] = access_token.access_token
-          end   
+          end
         end
         return return_hash
       end
@@ -42,7 +42,8 @@ module Sorcery
       # calculates and returns the url to which the user should be redirected,
       # to get authenticated at the external provider's site.
       def login_url(params, session)
-        authorize_url()
+        options = self.state.nil? ? {} : { state: self.state }
+        authorize_url(options)
       end
 
       # tries to login the user from access token
@@ -51,7 +52,7 @@ module Sorcery
           a[:code] = params[:code] if params[:code]
         end
 
-        res = get_access_token(args, token_url: token_url, token_method: :post)      
+        res = get_access_token(args, token_url: token_url, token_method: :post)
         return res
       end
 
@@ -64,7 +65,7 @@ module Sorcery
 
       def authorize_url(options = {})
         client = build_client(options)
-        authorization_uri = client.authorization_uri()        
+        authorization_uri = client.authorization_uri(options)
       end
 
       def get_access_token(args, options = {})
@@ -74,12 +75,12 @@ module Sorcery
             access_token = client.access_token! :body
           else
             access_token = client.access_token!
-          end          
+          end
 
           if @use_stored_jwk
             jwk = @stored_jwk
-          else 
-            jwk = get_public_keys()       
+          else
+            jwk = get_public_keys()
           end
           id_token = OpenIDConnect::ResponseObject::IdToken.decode(access_token.id_token, jwk)
           return {access_token: access_token, id_token: id_token}
@@ -94,10 +95,10 @@ module Sorcery
             response = conn.get('')
             json = JSON.parse(response.body)
             jwks_url = json['jwks_uri']
-          else 
+          else
             jwks_url = @jwks_uri
           end
-          
+
           conn = Faraday.new(jwks_url)
           response = conn.get('')
           jwk_set = JSON::JWK::Set.new(
@@ -105,8 +106,8 @@ module Sorcery
                           response.body
                         )
                       )
-          return jwk_set  
-          
+          return jwk_set
+
       end
 
       def build_client(options = {})
