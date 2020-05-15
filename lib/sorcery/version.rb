752d6cf68333df887364bb37f42db810e09646c0
Kamil Politowicz <kpolitowicz@aurorasolar.com>
2020-05-15 10:13:58 -0400

    gem: bumped version to 0.15.0
    To signal that the secuiry potch has been applied.

--- lib/sorcery/version.rb
+++ lib/sorcery/version.rb
@@ -1,3 +1,7 @@
 module Sorcery
-  VERSION = '0.14.0'.freeze
+  # This is not the same version as main sorcery repo.
+  # Compared to 0.14.0 it only fixes security vulnerability.
+  # The version is bumped for dependency checks to recognize this security patch.
+  # TODO: we should use main sorcery and sorcery-jwt gems, not private forks
+  VERSION = '0.15.0'.freeze
 end
